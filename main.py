dc94438db2bea0eafa636193b0c026a59edeb24b
bakanaouji <abeken1224@yahoo.co.jp>
2018-07-14 19:14:56 +0900

    add sample()

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+import numpy as np
 import subprocess as sp
 
 
-def evaluate():
-    sp.run('clang-format -i sample_src/input.h', stdout=sp.PIPE)
+def evaluate(x):
+    sp.run('clang-format -i -style="' + str(x) + '" sample_src/input.h', stdout=sp.PIPE)
     ret_val = sp.run('git diff --numstat sample_src/input.h', stdout=sp.PIPE)
     sp.run('git checkout sample_src/input.h', stdout=sp.PIPE)
     ret_val = ret_val.stdout.decode('utf-8').split('\t')
     return int(ret_val[0]) + int(ret_val[1])
 
 
+def sample():
+    styles = {}
+    styles['ColumnLimit'] = np.random.randint(80)
+    return str(styles)
+
+
 def main():
-    eval = evaluate()
+    x = sample()
+    eval = evaluate(x)
     print(eval)
 
 
