1909347d3d923946358aaa8d062f25dc4d131ad8
bakanaouji <abeken1224@yahoo.co.jp>
2018-07-14 18:19:50 +0900

    modify main.py

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import subprocess as sp
 
 
 def main():
-    sp.call('clang-format sample_src/input.h', shell=True)
+    sp.run('clang-format -i sample_src/input.h', stdout=sp.PIPE)
+    ret_val = sp.run('git diff --numstat sample_src/input.h', stdout=sp.PIPE)
+    sp.run('git checkout sample_src/input.h', stdout=sp.PIPE)
+    print(ret_val.stdout)
 
 
 if __name__ == '__main__':
