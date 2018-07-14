3a3380af70ba807fb248965fb990c234f2678bd8
bakanaouji <abeken1224@yahoo.co.jp>
2018-07-14 18:43:51 +0900

    add evaluate

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import subprocess as sp
 
 
-def main():
+def evaluate():
     sp.run('clang-format -i sample_src/input.h', stdout=sp.PIPE)
     ret_val = sp.run('git diff --numstat sample_src/input.h', stdout=sp.PIPE)
     sp.run('git checkout sample_src/input.h', stdout=sp.PIPE)
-    print(ret_val.stdout)
+    ret_val = ret_val.stdout.decode('utf-8').split('\t')
+    return int(ret_val[0]) + int(ret_val[1])
+
+
+def main():
+    eval = evaluate()
+    print(eval)
 
 
 if __name__ == '__main__':
