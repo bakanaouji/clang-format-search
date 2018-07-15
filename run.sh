e1d36161ef9419ee3e9b5400400279bd937a46c8
bakanaouji <abeken1224@yahoo.co.jp>
2018-07-16 06:40:00 +0900

    re reset

--- run.sh
+++ run.sh
@@ -5,7 +5,9 @@ git add .gitignore
 git add target_code
 cd src
 python hill_climbing_main.py
+cd ../
 git checkout .
+git reset HEAD target_code
 git checkout master
 git reset HEAD --hard
 git branch -D search
