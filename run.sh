3550f037f65baf37ae44fe6e13e611edf59a9c1a
bakanaouji <abeken1224@yahoo.co.jp>
2018-07-16 07:13:51 +0900

    add ga random_search to run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,13 @@ cat .gitignore_search > .gitignore
 git add .gitignore
 git add target_code
 cd src
-python hill_climbing_main.py
+if [ $1 = "hill_climbing" ]; then
+    python hill_climbing_main.py
+elif [ $1 = "ga" ]; then
+    python ga_main.py
+elif [ $1 = "random"]; then
+    python random_search_main.py
+fi
 cd ../
 git checkout .
 git reset HEAD target_code
