dfceb86e176e08cb2a8c824da915b84152543597
bakanaouji <abeken1224@yahoo.co.jp>
2018-07-16 06:19:30 +0900

    change to reset HEAD in run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ git checkout -b search
 cat .gitignore_search > .gitignore
 git add .gitignore
 git add target_code
-git commit -m "add target_code"
 cd src
 python hill_climbing_main.py
-git checkout target_code
+git reset HEAD .
+git checkout .
 git checkout master
 git branch -D search
