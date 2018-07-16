265f9f65bbba25f135466261c4f75ed67be23c4c
bakanaouji <abeken1224@yahoo.co.jp>
2018-07-16 15:52:56 +0900

    fix run.sh

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ if [ $1 = "hill_climbing" ]; then
     python hill_climbing_main.py
 elif [ $1 = "ga" ]; then
     python ga_main.py
-elif [ $1 = "random"]; then
+elif [ $1 = "random" ]; then
     python random_search_main.py
 fi
 cd ../
