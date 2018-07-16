9c6fca587a0f400740a7fe5aef3a519df2778463
bakanaouji <abeken1224@yahoo.co.jp>
2018-07-16 21:02:35 +0900

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,26 +1,9 @@
 #!/bin/sh
-# if [ $# -ne 1 ]; then
-#     echo "Please specify search algorithm name as argument."
-#     echo "algorithm name: hill_climbing, ga, random"
-#     exit 1
-# fi
-# if [ $1 != "hill_climbing" -a $1 != "ga" -a $1 != "random" ]; then
-#     echo "Please specify the correct search algorithm name as argument."
-#     echo "algorithm name: hill_climbing, ga, random"
-#     exit 1
-# fi
 git checkout -b search
 cat .gitignore_search > .gitignore
 git add .gitignore
 git add target_code
 cd src
-# if [ $1 = "hill_climbing" ]; then
-#     python hill_climbing_main.py
-# elif [ $1 = "ga" ]; then
-#     python ga_main.py
-# elif [ $1 = "random" ]; then
-#     python random_search_main.py
-# fi
 python main.py
 cd ../
 git reset HEAD .gitignore
