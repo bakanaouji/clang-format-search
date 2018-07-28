4b2592897e8722f689d090e4cd71a7a12f889ce4
bakanaouji <abeken1224@yahoo.co.jp>
2018-07-29 00:41:29 +0900

    divide run_search.py

--- src/run_search.py
+++ src/run_search.py
@@ -1,90 +1,30 @@
 import json
-import os
-import pandas as pd
-import subprocess as sp
 
-from copy import deepcopy
-from function import Function, style_maps, convert
-from optimizer.ga.ga import GA
-from optimizer.hill_climbing.hill_climbing import HillClimbing
-from optimizer.random.random_search import RandomSearch
+from function import Function, style_maps
+from searcher import Searcher
 
 
-def main():
-    # initializer parameter of experiment
+def initialize_parameter():
     with open('../config.json') as f:
         config = json.load(f)
     params = {
         'maps': style_maps(),
         'obj_func': Function('../target_code', '../default-style.json'),
+        'optimizer': config['optimizer'],
         'path': '../log/' + config['optimizer'],
         'max_evals': config['max_evals'],
         'pop_size': config['ga_pop_size'],
         'tournament_size': config['ga_tournament_size'],
         'pipe_to_hill_climbing': config['ga_pipe_to_hill_climbing']
     }
+    return params
 
-    # initialize optimizer
-    optimizer = None
-    optimizer_name = config['optimizer']
-    if optimizer_name == 'ga':
-        optimizer = GA
-    elif optimizer_name == 'hill_climbing':
-        optimizer = HillClimbing
-    elif optimizer_name == 'random':
-        optimizer = RandomSearch
-    optimizer = optimizer(**params)
 
-    if not os.path.isdir(params['path']):
-        os.makedirs(params['path'])
+def main():
+    params = initialize_parameter()
 
-    # optimize
-    log = {'g': [], 'evals': [], 'fval': []}
-    g_offset, evals_offset = 0, 0
-    while True:
-        optimizer.one_iteration()
-        # stock log
-        log['g'].append(optimizer.g + g_offset)
-        log['evals'].append(optimizer.evals + evals_offset)
-        log['fval'].append(optimizer.fval)
-        print(optimizer.g + g_offset, optimizer.evals + evals_offset,
-              optimizer.fval, optimizer.styles)
-        if optimizer.evals >= params['max_evals'] or optimizer.done:
-            if optimizer_name == 'ga' and params['pipe_to_hill_climbing']:
-                print('best:', optimizer.best_fval)
-                print('best styles:', optimizer.best_styles)
-                print('pipe to hill climbing')
-                params['pipe_to_hill_climbing'] = False
-                params['initial_styles'] = optimizer.styles
-                g_offset = optimizer.g
-                evals_offset = optimizer.evals
-                optimizer = HillClimbing(**params)
-            else:
-                break
-
-    # save log
-    df = pd.DataFrame(log)
-    df.index.name = '#index'
-    df.to_csv('%s/log.csv' % params['path'], sep=',')
-    best_info = {
-        'best_styles': optimizer.best_styles,
-        'best': optimizer.best_fval
-    }
-    json.dump(best_info, open('%s/best.json' % params['path'], 'w'))
-    default_style = {}
-    if os.path.exists('../default-style.json'):
-        with open('../default-style.json') as f:
-            default_style = json.load(f)
-    best_styles = deepcopy(optimizer.best_styles)
-    best_styles.update(default_style)
-    best_styles = str(convert(best_styles))
-    ret_val = sp.run('clang-format -style="' + best_styles + '" -dump-config',
-                     stdout=sp.PIPE).stdout.decode('utf-8')
-    with open('%s/.clang-format' % params['path'], 'w') as f:
-        f.write(ret_val)
-
-    print('best:', optimizer.best_fval)
-    print('best styles:', optimizer.best_styles)
+    searcher = Searcher(**params)
+    searcher.optimize()
 
 
 if __name__ == '__main__':
---
+++ src/searcher.py
@@ -0,0 +1,79 @@
+import json
+import os
+import pandas as pd
+import subprocess as sp
+
+from copy import deepcopy
+from function import convert
+from optimizer.ga.ga import GA
+from optimizer.hill_climbing.hill_climbing import HillClimbing
+from optimizer.random.random_search import RandomSearch
+
+
+class Searcher(object):
+    def __init__(self, **params):
+        self.params = params
+        self.optimizer = self.initialize_optimizer()
+        if not os.path.isdir(self.params['path']):
+            os.makedirs(self.params['path'])
+
+    def initialize_optimizer(self):
+        optimizer = None
+        optimizer_name = self.params['optimizer']
+        if optimizer_name == 'ga':
+            optimizer = GA
+        elif optimizer_name == 'hill_climbing':
+            optimizer = HillClimbing
+        elif optimizer_name == 'random':
+            optimizer = RandomSearch
+        return optimizer(**self.params)
+
+    def optimize(self):
+        # optimize
+        log = {'g': [], 'evals': [], 'fval': []}
+        g_offset, evals_offset = 0, 0
+        while True:
+            self.optimizer.one_iteration()
+            # stock log
+            log['g'].append(self.optimizer.g + g_offset)
+            log['evals'].append(self.optimizer.evals + evals_offset)
+            log['fval'].append(self.optimizer.fval)
+            print(self.optimizer.g + g_offset, self.optimizer.evals + evals_offset,
+                  self.optimizer.fval, self.optimizer.styles)
+            if self.optimizer.evals >= self.params['max_evals'] or self.optimizer.done:
+                if self.params['optimizer'] == 'ga' \
+                        and self.params['pipe_to_hill_climbing']:
+                    print('best:', self.optimizer.best_fval)
+                    print('best styles:', self.optimizer.best_styles)
+                    print('pipe to hill climbing')
+                    self.params['pipe_to_hill_climbing'] = False
+                    self.params['initial_styles'] = self.optimizer.styles
+                    g_offset = self.optimizer.g
+                    evals_offset = self.optimizer.evals
+                    self.optimizer = HillClimbing(**self.params)
+                else:
+                    break
+
+        # save log
+        df = pd.DataFrame(log)
+        df.index.name = '#index'
+        df.to_csv('%s/log.csv' % self.params['path'], sep=',')
+        best_info = {
+            'best_styles': self.optimizer.best_styles,
+            'best': self.optimizer.best_fval
+        }
+        json.dump(best_info, open('%s/best.json' % self.params['path'], 'w'))
+        default_style = {}
+        if os.path.exists('../default-style.json'):
+            with open('../default-style.json') as f:
+                default_style = json.load(f)
+        best_styles = deepcopy(self.optimizer.best_styles)
+        best_styles.update(default_style)
+        best_styles = str(convert(best_styles))
+        ret_val = sp.run('clang-format -style="' + best_styles + '" -dump-config',
+                         stdout=sp.PIPE).stdout.decode('utf-8')
+        with open('%s/.clang-format' % self.params['path'], 'w') as f:
+            f.write(ret_val)
+
+        print('best:', self.optimizer.best_fval)
+        print('best styles:', self.optimizer.best_styles)
